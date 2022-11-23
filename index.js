161dbae025951d4433458738563a68950daf282e
Student <venus.kabera@student.moringaschool.com>
2022-11-23 23:32:03 +0300

    completed assignments

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution in this file!
+const drivers = ['job', 'rob', 'tim', 'roy'];
+
+const returnFirstTwoDrivers = function(drivers) {
+  return drivers.slice(0,2);
+}
+
+const returnLastTwoDrivers = function (drivers) {
+  return drivers.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers];
+
+const createFareMultiplier = function (fareMultiplier) {
+  return function(value) {
+    return fareMultiplier * value;
+  }
+}
+
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+function fetchSpecifiedDrivers(drivers, func) {
+  return func(drivers);
+}
+
+function selectDifferentDrivers(driversArray, fn) {
+    return fn(driversArray);
+}
